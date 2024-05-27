7918f231b91a3e57d1c9c838f5d91a10bb3dd42c
yasmeenrahimhuda <155715580+yasmeenrahimhuda@users.noreply.github.com>
2024-05-27 07:43:48 +0500

    Add files via upload

---
+++ 09 chapter14-16/app.js
@@ -0,0 +1,118 @@
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+var student=[];
+
+
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+var student=[]
+
+
+// 3. Declare and initialize a strings array.
+
+var studentString=["hina,bina,tina"]
+
+
+// 4. Declare and initialize a numbers array.
+
+var studentNumbers=[4,6,8,10]
+
+// 5. Declare and initialize a boolean array.
+var boolean=[true, false, true, false];
+
+
+// 6. Declare and initialize a mixed array.
+
+var mixedArray = [42, "hello", true, null, { studentname: "hina" }, [1, 2, 3]];
+
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+var eduQualification = (SSC, HSC, BCS, BS, BCOM, MS, PhD );
+
+document.write=(SSC, HSC, BCS, BS, BCOM, MS, PhD );
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+var studentName=["Hina","Bina","Tina"]
+var score=[350, 300, 400]
+var totalScore=500
+
+
+// 9. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then
+// remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+
+
+
+
+
+// 10. Write a program to store student scores in an array &
+// sort the array in ascending order using Array’s sort
+// method.
+
+
+// 11. Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array.
+
+var city0 = "karachi";
+var city1 = "lahore";
+var city2 = "peshawar";
+
+
+
+// 12. Write a program to create a single string from the
+// below mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+
+
+
+
+
+
+// 13. Create a new array. Store values one by one in such a way
+// that you can access the values in the order in which they
+// were stored. (FIFO-First In First Out)
+
+
+
+
+// 14. Create a new array. Store values one by one in such a way
+// that you can access the values in reverse order. (Last In-
+// First Out)
+
+
+
+// 15. Write a program to store phone manufacturers (Apple,
+//   Samsung, Motorola, Nokia, Sony & Haier) in an array.
+//   Display the following dropdown/select menu in your
+//   browser using document.write() method:
+ // Declare and initialize the array with phone manufacturers
+ 
